dc2247393cb90a0a6145fde8ea7dfa70f5465c4a
issamkn <161899909+issamkn@users.noreply.github.com>
2024-03-12 21:08:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from hangman_art import logo, stages
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
-print(chosen_word)
 
 display = []
 for letter in chosen_word:
@@ -11,9 +10,9 @@ for letter in chosen_word:
 
 lives = 6
 end_of_game = False
+
 while not end_of_game:
-
-
+  
   guess = input("Guess a letter: ").lower()
   if guess in display:
     print(f"You have already guessed {guess}")
